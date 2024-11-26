1d6fe3fe6838f848544144642e6fbea71c57c4eb
AbdelrahmanBahig <abdelrahmanbahig04@gmail.com>
2024-11-26 14:03:57 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages 
+
+setup(
+    name="NotificationManager",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Abdelrahman Bahig",
+    description="A simple notification manager package",
+    long_description=open("ReadME.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/AbdelrahmanBahig/NotificationManager",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
