d2dc20abf443cca4eb387e39314180bd3e5d6f56
AbdelrahmanBahig <abdelrahmanbahig04@gmail.com>
2024-11-27 11:35:27 +0200

    Initial commit

--- notification_manager/__init__.py
+++ notification_manager/__init__.py
@@ -1,7 +1,4 @@
 from notification_manager.notification import Notification
 from notification_manager.system_notifications import SystemNotification
 
-__all__ =["Notification", "SystemNotification"]git init
-git add .
-git commit -m "Initial commit"
-git remote add origin https://github.com/AbdelrahmanBahig/NotificationManager.git
+__all__ =["Notification", "SystemNotification"]
--- notification_manager/system_notifications.py
+++ notification_manager/system_notifications.py
@@ -7,7 +7,7 @@ class SystemNotification():
     
     def send_notification(self,user_id ,message):
         
-        notification = Notification.Notification(user_id,message)
+        notification = Notification(user_id,message)
         self.notifications.append(notification)
 
     def view_notification(self , user_id, message):
@@ -21,19 +21,14 @@ class SystemNotification():
     
 
     def mark_notifiction_as_read(self, user_id, message):
-        notification = self.get_notification(user_id,message)
-        notification.mark_as_read()
-        return notification
+         notification = self.view_notification(user_id,message)
+         notification.mark_as_read()
+         return notification
     
     def show_number_of_unread_notifications(self):
         return Notification.Notification.number_of_notifictions
     
 
-n1 = SystemNotification()
-n1.create_notification(1, "hellow")
-n1.create_notification(2, "welcom")
-n1.mark_notifiction_as_read(2,"welcom")
-print(n1.show_number_of_unread_notifications())
 
 
 
